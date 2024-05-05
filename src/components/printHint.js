77fdc17100105576a631e980722a50194f4cceb5
userjmmm <a@as-MacBook-Pro.local>
2024-05-06 03:36:11 +0900

    FEAT: 숫자를 모두 맞히면 게임이 종료 메시지와 재시작 버튼이 나오는 기능

--- src/components/printHint.js
+++ src/components/printHint.js
@@ -5,13 +5,13 @@ export function classifyHint({ strike, ball}) {
         resultElement.textContent = '🎉정답을 맞히셨습니다🎉'
     }
     else if (strike !== 0 && ball === 0) {
-        resultElement.textContent = `${strike} 스트라이크`
+        resultElement.textContent = `${strike}스트라이크`
     }
     else if (ball !== 0 && strike === 0) {
-        resultElement.textContent = `${ball} 볼`
+        resultElement.textContent = `${ball}볼`
     }
     else if (ball != 0 && strike != 0) {
-        resultElement.textContent = `${ball} 볼 ${strike} 스트라이크`
+        resultElement.textContent = `${ball}볼 ${strike}스트라이크`
     }
     else {
         resultElement.textContent = '낫싱'
--- src/main.js
+++ src/main.js
@@ -3,13 +3,31 @@ import { checkInput } from './components/numberInput.js';
 import { calculateHint } from './components/makeHint.js';
 import { classifyHint } from './components/printHint.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const comRandom = threeRandom()
-    console.log('상대방(컴퓨터)의 수: ', comRandom)
+const gameState = {
+    comRandom: null
+}
+// 숫자를 모두 맞히면 게임이 종료 메시지와 재시작 버튼이 나오는 기능
+function setRestartButton() {
+    const restartButton = document.getElementById('restartButton')
+    restartButton.addEventListener('click', () => window.location.reload())
+}
 
-    document.getElementById('checkButton').addEventListener('click', () => {
-        const userInput = checkInput()
-        const calHint = calculateHint(userInput, comRandom)
-        classifyHint(calHint)
-    });
+function gameResult() {
+    const userInput = checkInput()
+    const calHint = calculateHint(userInput, gameState.comRandom)
+    classifyHint(calHint)
+
+    const resultElement = document.getElementById('result')
+    if (resultElement.textContent.includes('🎉정답을 맞히셨습니다🎉')) {
+        const restartButton = document.getElementById('restartButton')
+        restartButton.style.display = 'block'
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    gameState.comRandom = threeRandom()
+    console.log('상대방(컴퓨터)의 수: ', gameState.comRandom)
+
+    document.getElementById('checkButton').addEventListener('click', gameResult)
+    setRestartButton()
 });
