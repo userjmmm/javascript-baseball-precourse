bf15f0f73abdf1e9b4a1d5af57a41e28a733ca09
userjmmm <a@as-MacBook-Pro.local>
2024-05-06 02:46:08 +0900

    FEAT: 확인 버튼을 누르면 힌트를 제공하는 기능

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import { threeRandom } from './components/makeRandom.js';
 import { checkInput } from './components/numberInput.js';
 import { calculateHint } from './components/makeHint.js';
+import { classifyHint } from './components/printHint.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const comRandom = threeRandom();
-    console.log('상대방(컴퓨터)의 수: ', comRandom);
+    const comRandom = threeRandom()
+    console.log('상대방(컴퓨터)의 수: ', comRandom)
 
     document.getElementById('checkButton').addEventListener('click', () => {
-        const userInput = checkInput();
-        calculateHint(userInput, comRandom);
+        const userInput = checkInput()
+        const calHint = calculateHint(userInput, comRandom)
+        classifyHint(calHint)
     });
 });
