799efe777974076492f1e51aa08d3c188a60c0c1
userjmmm <a@as-MacBook-Pro.local>
2024-05-05 23:11:48 +0900

    FEAT: 잘못된 값을 넣은 경우, 에러 메시지 출력 후 그 부분부터 다시 입력받는 기능 추가

--- src/components/numberInput.js
+++ src/components/numberInput.js
@@ -1,12 +1,14 @@
+import { inputError } from './rewriteInput.js';
+
+export const userInput = document.getElementById('userInput')
+
 export function checkInput() { // 잘못된 값 입력하는지 검증
-    const userInput = document.getElementById('userInput').value
-    const numbers = userInput.split('').map(Number)
+    const numbers = userInput.value.split('').map(Number)
     const isValid = numbers.length === 3 && numbers.every(num => num>=1 && num<=9 && Number.isInteger(num))
     const isDuplicate = new Set(numbers).size === numbers.length
 
     if (!isValid || !isDuplicate) {
-        alert('잘못된 값을 입력했습니다. 다시 입력해 주세요!')
-        return;
+        inputError()
     }
     console.log('입력 받은 숫자: ', numbers);
 }
